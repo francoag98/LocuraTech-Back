21c6d9fd6501fe6c9063987ada9bb77f8506878e
Franco Aglieri <francoaglieri@hotmail.com>
2023-06-06 15:49:03 -0300

    new init project

---
+++ src/main/java/com/LocuraTech/locuraInit/LocuraInitApplication.java
@@ -0,0 +1,13 @@
+package com.LocuraTech.locuraInit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocuraInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocuraInitApplication.class, args);
+	}
+
+}
