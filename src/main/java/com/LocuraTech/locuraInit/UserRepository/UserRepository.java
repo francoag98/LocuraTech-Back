a568112dcd4667aa0719e257c67ac64475d5e224
francoag98 <francoaglieri@hotmail.com>
2023-06-07 12:26:25 -0300

    getbyname

--- src/main/java/com/LocuraTech/locuraInit/UserRepository/UserRepository.java
+++ src/main/java/com/LocuraTech/locuraInit/UserRepository/UserRepository.java
@@ -2,7 +2,10 @@ package com.LocuraTech.locuraInit.UserRepository;
 
 import com.LocuraTech.locuraInit.Models.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 public interface UserRepository extends MongoRepository<UserModel, String> {
