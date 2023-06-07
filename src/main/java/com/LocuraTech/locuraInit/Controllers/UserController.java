08ca6dc64263c36dee8b62db2da70e1be1324d47
Franco Aglieri <francoaglieri@hotmail.com>
2023-06-06 22:46:33 -0300

    first routes

---
+++ src/main/java/com/LocuraTech/locuraInit/Controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.LocuraTech.locuraInit.Controllers;
+
+import com.LocuraTech.locuraInit.Models.UserModel;
+import com.LocuraTech.locuraInit.Services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserServices userServices;
+
+    @GetMapping
+    public ResponseEntity<?> getUsers(){
+        try {
+            ArrayList<UserModel> users = userServices.getUsers();
+            if (users.isEmpty()) {
+                return new ResponseEntity<String>("no content", HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<ArrayList<UserModel>>(users, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody UserModel user){
+        try {
+            String password = user.getPassword();
+            UserModel userCreated = userServices.saveUser(user);
+            return new ResponseEntity<UserModel>(user, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
--- src/main/java/com/LocuraTech/locuraInit/UserRepository/UserRepository.java
+++ src/main/java/com/LocuraTech/locuraInit/UserRepository/UserRepository.java
@@ -2,7 +2,9 @@ package com.LocuraTech.locuraInit.UserRepository;
 
 import com.LocuraTech.locuraInit.Models.UserModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends MongoRepository<UserModel, Long> {
+@Repository
+public interface UserRepository extends MongoRepository<UserModel, String> {
 
 }
